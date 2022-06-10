acf91e0747e205352b2b4610486bba6867e743fb
Alexandre Gaglianone <lelepg22@hotmail.com>
2022-06-10 11:17:38 +0200

    structure

---
+++ Hierarchie de classes/Hierarchie de classes/Truck Template.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchie_de_classes
+{
+    public class Truck_Template
+    {
+    }
+}
