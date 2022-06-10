39d0b4561d2fdf89ec057cd208b01124f19aca5d
Alexandre Gaglianone <lelepg22@hotmail.com>
2022-06-10 11:05:12 +0200

    init

---
+++ Hierarchie de classes/Hierarchie de classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hierarchie_de_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
